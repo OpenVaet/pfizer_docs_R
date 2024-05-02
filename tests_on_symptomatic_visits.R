f32c5c5e9727c49d37f75e16268cb0e56f17a573
OpenVAET <openvaet@protonmail.com>
2024-05-02 10:54:00 +0200

    Pfizer Documents - v 1.23.16 - Symptomatic Visits WIP

--- tests_on_symptomatic_visits.R
+++ tests_on_symptomatic_visits.R
@@ -45,8 +45,8 @@ print(symptoms_sup_selected)
 
 # Creates a dataframe subjects_sympto_visits containing, for each unique SUBJID-VISIT pair in symptoms_selected
 subjects_sympto_visits <- symptoms_selected %>%
-  group_by(SUBJID, VISIT) %>%
-  summarize(
+  group_by(SUBJID, VISIT, ARM, SITEID) %>%
+  summarise(
     EARLIESTSYMPTDT = min(ADT, na.rm = TRUE),
     .groups = "drop"
   ) %>%
@@ -62,6 +62,11 @@ subjects_sympto_visits <- symptoms_selected %>%
   ) %>%
   select(-VISIT.y)
 
+# Removes the VISIT.x column from the subjects_sympto_visits dataframe
+subjects_sympto_visits <- subjects_sympto_visits %>%
+  select(-VISIT.x)
+
+print(subjects_sympto_visits)
 
 # Displays the exceptions where EARLIESTSYMPTDT is not greater than or equal to EARLIESTDT
 exceptions <- subjects_sympto_visits[subjects_sympto_visits$EARLIESTSYMPTDT < subjects_sympto_visits$EARLIESTDT, ]
@@ -80,22 +85,102 @@ if (nrow(exceptions) > 0) {
 } else {
   cat("No exceptions found. EARLIESTSYMPTDT is greater than or equal to EARLIESTDT for all rows.\n")
 }
-
-# Removes the VISIT.x column from the subjects_sympto_visits dataframe
-subjects_sympto_visits <- subjects_sympto_visits %>%
-  select(-VISIT.x)
+print(subjects_sympto_visits)
 
 # Reformats the subjects_sympto_visits dataframe to ensure each row is unique
 subjects_sympto_visits <- subjects_sympto_visits %>%
-  group_by(SUBJID, VISIT) %>%
+  group_by(SUBJID, ARM, SITEID, VISIT) %>%
   summarize(
     EARLIESTSYMPTDT = min(EARLIESTSYMPTDT),
     FAORRES = if (all(is.na(FAORRES))) NA else min(FAORRES, na.rm = TRUE),
     EARLIESTDT = min(EARLIESTDT),
     .groups = "drop"
   )
+print(subjects_sympto_visits)
 
+# Filters out rows where EARLIESTDT is after 2021-03-13
+subjects_sympto_visits <- subjects_sympto_visits %>%
+  filter(ymd(EARLIESTDT) <= ymd("2021-03-13"))
+
+# Removes the EARLIESTSYMPTDT column from the subjects_sympto_visits dataframe
+subjects_sympto_visits <- subjects_sympto_visits %>%
+  select(-EARLIESTSYMPTDT)
+
+print(subjects_sympto_visits)
+
+# Loads tests file.
+tests_file <- "xpt_data/FDA-CBER-2021-5683-0282366 to -0285643_125742_S1_M5_c4591001-S-D-mb.xpt"
+tests <- read_xpt(tests_file)
+
+# Filters the tests dataframe
+tests <- tests %>%
+  filter(MBTEST %in% c("Cepheid RT-PCR assay for SARS-CoV-2", "SEVERE ACUTE RESP SYNDROME CORONAVIRUS 2"))
+tests <- tests %>%
+  mutate(SUBJID = str_extract(USUBJID, "\\d+$"))
+print(colnames(tests))
+print(tests)
+
+# Sustains only columns required for the current analysis.
+tests_selected <- tests[c("SUBJID", "VISIT", "MBTEST", "MBORRES", "MBDTC")]
+
+# Replaces values in MBORRES column
+tests_selected$MBORRES <- replace(tests_selected$MBORRES, tests_selected$MBORRES == "NEGATIVE", "NEG")
+tests_selected$MBORRES <- replace(tests_selected$MBORRES, tests_selected$MBORRES == "POSITIVE", "POS")
+tests_selected$MBORRES <- replace(tests_selected$MBORRES, tests_selected$MBORRES == "INDETERMINATE", "IND")
+
+# Filters tests_selected to keep only rows where VISIT contains "COVID_"
+tests_selected <- tests_selected[grepl("COVID_", tests_selected$VISIT), ]
+print(tests_selected)
+
+# Filters out rows where MBDTC is after 2021-03-13
+tests_selected <- tests_selected %>%
+  filter(ymd(MBDTC) <= ymd("2021-03-13"))
+
+# Creates the central_test dataframe
+central_test = tests_selected[tests_selected$MBTEST == 'Cepheid RT-PCR assay for SARS-CoV-2', c('SUBJID', 'VISIT', 'MBORRES', 'MBDTC')]
+
+# Creates the local_test dataframe
+local_test = tests_selected[tests_selected$MBTEST == 'SEVERE ACUTE RESP SYNDROME CORONAVIRUS 2', c('SUBJID', 'VISIT', 'MBORRES', 'MBDTC')]
+
+print(central_test)
+print(local_test)
+print(subjects_sympto_visits)
+
+subjects_sympto_visits <- subjects_sympto_visits %>%
+  left_join(
+    central_test %>% 
+      group_by(SUBJID, VISIT) %>% 
+      summarise(.groups = "drop", 
+                EARLIESTCENTRALDT = min(MBDTC), 
+                EARLIESTCENTRALPOSDT = ifelse(length(MBDTC[MBORRES == "POS"]) == 0, NA, min(MBDTC[MBORRES == "POS"]))),
+    by = c("SUBJID", "VISIT")
+  ) %>%
+  left_join(
+    local_test %>% 
+      group_by(SUBJID, VISIT) %>% 
+      summarise(.groups = "drop", 
+                EARLIESTLOCALDT = min(MBDTC), 
+                EARLIESTLOCALPOSDT = ifelse(length(MBDTC[MBORRES == "POS"]) == 0, NA, min(MBDTC[MBORRES == "POS"]))),
+    by = c("SUBJID", "VISIT")
+  )
+subjects_sympto_visits <- subjects_sympto_visits %>%
+  select(-FAORRES)
+print(subjects_sympto_visits)
+
+# Adds the total of days between the symptoms & the test
+subjects_sympto_visits <- subjects_sympto_visits %>%
+  mutate(
+    EARLIESTCENTRALDAYSTOSYMPT = ifelse(!is.na(EARLIESTCENTRALDT), as.numeric(as.Date(EARLIESTCENTRALDT) - as.Date(EARLIESTDT)), NA),
+    EARLIESTCENTRALPOSDAYSTOSYMPT = ifelse(!is.na(EARLIESTCENTRALPOSDT), as.numeric(as.Date(EARLIESTCENTRALPOSDT) - as.Date(EARLIESTDT)), NA),
+    EARLIESTLOCALDAYSTOSYMPT = ifelse(!is.na(EARLIESTLOCALDT), as.numeric(as.Date(EARLIESTLOCALDT) - as.Date(EARLIESTDT)), NA),
+    EARLIESTLOCALPOSDAYSTOSYMPT = ifelse(!is.na(EARLIESTLOCALPOSDT), as.numeric(as.Date(EARLIESTLOCALPOSDT) - as.Date(EARLIESTDT)), NA)
+  )
 print(subjects_sympto_visits)
 
 # Writes the result to a CSV file
 write.csv(subjects_sympto_visits, "phase_3_subjects_sympto_visits.csv", row.names = FALSE)
+
+
+
+unique_arms <- unique(tests_selected$VISIT)
+print(unique_arms)
