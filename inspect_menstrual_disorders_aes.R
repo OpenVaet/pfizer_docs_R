113e17c7253f592e31bc0206701ff87887896c97
OpenVAET <openvaet@protonmail.com>
2024-05-17 09:52:16 +0200

    Pfizer Documents - v 1.25.44 - Subjects medical history

--- inspect_menstrual_disorders_aes.R
+++ inspect_menstrual_disorders_aes.R
@@ -2,6 +2,7 @@
 library(haven)
 library(dplyr)
 library(tidyr)
+library(readr)
 
 # ADAE
 adae_path <- 'xpt_data/FDA-CBER-2021-5683-0774873-0775804_125742_S1_M5_C4591001-A-D_adae.xpt'
@@ -11,9 +12,33 @@ if (!file.exists(adae_path)) {
 
 # Reads & filters the ADAE file.
 adae_data <- read_xpt(adae_path)
-adae_selected_data <- adae_data[c("SUBJID", "USUBJID", "VPHASE", "APERIODC", "AREL", "AEHLGT", "AEHLT", "AESER", "AERELTXT", "ATOXGR", "AESTDTC", "AEENDTC", "AEDECOD")]
+adae_selected_data <- adae_data[c("SUBJID", "ARM", "VPHASE", "AREL", "AERELTXT", "AESTDTC", "AEENDTC", "AEDECOD")]
 print(adae_selected_data)
 adae_data_filtered <- adae_selected_data %>%
   filter(AEDECOD %in% c("Menorrhagia"))
 print(unique(adae_data_filtered$AEDECOD))
+print('Subjects reporting Menorrhagia :')
 print(adae_data_filtered)
+
+# Filters Menorrhagia subjects to the BNT only.
+bnt_adae_data <- adae_data_filtered %>%
+  filter(ARM == "BNT162b2 Phase 2/3 (30 mcg)")
+print(bnt_adae_data)
+
+# Loads the Phase 3 population randomized.
+randomized_pop_file <- 'phase_3_randomized_pop.csv'
+randomized_pop <- read.csv(randomized_pop_file)
+women_randomized_pop <- randomized_pop %>%
+  filter(ARM == "BNT162b2 Phase 2/3 (30 mcg)")
+women_randomized_pop <- women_randomized_pop %>%
+  filter(AGE >= 16 & AGE <= 39 & SEX == 'F')
+print(women_randomized_pop)
+
+total_bnt_menorrhagia <- nrow(bnt_adae_data)
+total_bnt <- nrow(women_randomized_pop)
+print(total_bnt_menorrhagia)
+print(total_bnt)
+menorrhagia_rate <- total_bnt_menorrhagia * 100 / total_bnt
+print(paste('Rates of Menorrhagia / 100.000 : ', menorrhagia_rate))
+
+      
--- parse_medical_history.R
+++ parse_medical_history.R
@@ -10,8 +10,44 @@ print(medical_history)
 # Extracts the subject_id (unique identifier by subject which doesn't change as the subject changes sites) from the USUBJID
 medical_history$SUBJID <- sub(".*C4591001 \\d{4} (\\d{8}).*", "\\1", medical_history$USUBJID)
 
-medical_history_filtered <- medical_history %>%
-  filter(SUBJID %in% c("10071087", "10011061", "11281119", "12313161", "12641025"))
-print(unique(medical_history$MHDECOD))
-print(medical_history_filtered, n=30)
-write.csv(unique(medical_history$MHDECOD), 'medical_history_decod.csv')
+# Loads the Phase 3 population randomized.
+randomized_pop_file <- 'phase_3_randomized_pop.csv'
+randomized_pop <- read.csv(randomized_pop_file)
+randomized_pop <- randomized_pop %>%
+  filter(ARM == 'BNT162b2 Phase 2/3 (30 mcg)' & SEX == 'F')
+print(randomized_pop)
+medical_history_filtered <- medical_history[medical_history$SUBJID %in% randomized_pop$SUBJID, ]
+print(medical_history_filtered)
+
+medical_history_with_bleedings <- medical_history_filtered %>%
+  filter(MHDECOD %in% c(
+    "Menorrhagia"
+  ))
+medical_history_with_potential_bleedings <- medical_history_filtered %>%
+  filter(MHDECOD %in% c(
+    "Menorrhagia", "Dysfunctional uterine bleeding",
+    "Ovarian haemorrhage", "Vaginal cyst",
+    "Vulvovaginal pain", "Haemorrhage",
+    "Ovarian cyst", "Ovarian rupture",
+    "Ovarian cyst ruptured", "Ovarian cancer",
+    "Benign ovarian tumour", "Ovarian mass"
+  ))
+subjects_with_bleedings <- length((unique(medical_history_with_bleedings$SUBJID)))
+print(subjects_with_bleedings)
+print(length(subjects_with_bleedings))
+
+print(unique(medical_history_with_bleedings$MHENTPT)) 
+
+# Loads the Phase 3 population randomized.
+women_randomized_pop <- randomized_pop %>%
+  filter(AGE >= 16 & AGE <= 39 & SEX == 'F')
+total_bnt <- nrow(women_randomized_pop)
+
+# Calculates the rates or Menorrhagia among past history.
+menorrhagia_rate <- subjects_with_bleedings * 100 / total_bnt
+print(paste('Rates of Menorrhagia / 100.000 : ', menorrhagia_rate))
+
+# Calculates the rates of other troubles whihc could be used to classify Menorrhagia
+subjects_with_potential_bleedings <- length((unique(medical_history_with_potential_bleedings$SUBJID)))
+menorrhagia_and_others_rate <- subjects_with_potential_bleedings * 100 / total_bnt
+print(paste('Rates of Menorrhagia & other bleeding causes / 100.000 : ', menorrhagia_and_others_rate))
